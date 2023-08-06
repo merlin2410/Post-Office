3a887950bdcc86942afb3b945a4a919cfeb3de9f
Merlin Mon Mathew <merlinmonmathew@gmail.com>
2023-08-06 06:04:48 +0530

    fetch ip address

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// Using a third-party API to get the user's IP address
+ipAddressDisplay = document.getElementById('ip-address-display');
+fetch('https://api64.ipify.org?format=json')
+  .then(response => response.json())
+  .then(data => {
+    const ipAddress = data.ip;
+    ipAddressDisplay.innerText = `Your Current IP address is ${ipAddress}`;
+  })
+  .catch(error => {
+    console.error('Error fetching IP address:', error);
+  });
+
+  
+
+  
+  
